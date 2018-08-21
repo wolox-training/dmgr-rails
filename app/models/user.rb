42ee3e2f9af9774417a94510949a71a91f8663a3
Droche <diego.rochebouet@gmail.com>
2018-08-21 16:13:03 -0300

    Adds authentication files

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,11 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
   validates :first_name, :last_name, presence: true
+
+  # Hooks
+  before_validation :generate_verification_code, on: :create
+
+  def generate_verification_code
+    self.verification_code = AuthenticableEntity.verification_code
+  end
 end
