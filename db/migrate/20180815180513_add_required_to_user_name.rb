2d076b385797177dbb9a47c440c97d92abcd98c3
Droche <diego.rochebouet@gmail.com>
2018-08-21 16:43:39 -0300

    Merge the user name migration into one
    Merges the 20180815132933_add_name_to_user migration with 20180815180513_add_required_to_user_name.
    It now creates the user with the name as a requirement in a single migration.

--- db/migrate/20180815180513_add_required_to_user_name.rb
+++
@@ -1,6 +0,0 @@
-class AddRequiredToUserName < ActiveRecord::Migration[5.1]
-  def change
-    change_column_null :users, :first_name, false
-    change_column_null :users, :last_name, false
-  end
-end
