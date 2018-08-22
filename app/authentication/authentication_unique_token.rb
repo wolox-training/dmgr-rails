051861f6a9d639aac86b40fb2b8c813461d9e3bf
Diego de Rochebouet <diego.rochebouet@gmail.com>
2018-08-22 09:48:42 -0300

    Authentication (#5)
    -Adds authentication files
    -Session_controller now inherits from api_controller

---
+++ app/authentication/authentication_unique_token.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+# app/authentication/authentication_unique_token.rb
+class AuthenticationUniqueToken
+  class << self
+    # I'm not checking the uniqueness because its unlikely to happen
+    def generate
+      SecureRandom.hex(16)
+    end
+  end
+end
