850a41af3ba736fb82f78e4da204fb1dd2417797
Diego de Rochebouet <diego.rochebouet@gmail.com>
2018-08-27 17:09:00 -0300

    Creates rent model (#8)
    -Adds rent model
    -Adds rent migration
    -Adds 'has_many :rent' to books and users

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  has_many :rent, dependent: :destroy
+
   validates :first_name, :last_name, presence: true
 
   # Hooks
