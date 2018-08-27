6c4aee07672cc084a54fe19aea1554951add35a7
Droche <diego.rochebouet@gmail.com>
2018-08-27 10:41:18 -0300

    Fix all rubocop violations

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,9 +1,11 @@
+# frozen_string_literal: true
+
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
-require File.expand_path('../../config/environment', __FILE__)
+require File.expand_path('../config/environment', __dir__)
 # Prevent database truncation if the environment is production
-abort("The Rails environment is running in production mode!") if Rails.env.production?
+abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 
@@ -63,15 +65,14 @@ RSpec.configure do |config|
   config.render_views
 
   Shoulda::Matchers.configure do |config|
-  config.integrate do |with|
-    # Choose a test framework:
-    with.test_framework :rspec
-    #with.test_framework :minitest
-    #with.test_framework :minitest_4
-    #with.test_framework :test_unit
+    config.integrate do |with|
+      # Choose a test framework:
+      with.test_framework :rspec
+      # with.test_framework :minitest
+      # with.test_framework :minitest_4
+      # with.test_framework :test_unit
 
-    with.library :rails
+      with.library :rails
+    end
   end
-end
-
 end
