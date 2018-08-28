a03f971cd3a397013e7f0c6bb53568c4b568cee8
Droche <diego.rochebouet@gmail.com>
2018-08-28 17:39:07 -0300

    Deletes returned_at

--- app/serializers/book_rent_serializer.rb
+++ app/serializers/book_rent_serializer.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class BookRentSerializer < ActiveModel::Serializer
-  attributes :id, :from, :to, :book_id, :user_id, :returned_at
+  attributes :id, :from, :to, :book_id, :user_id
 
   # has_many :rent
 
