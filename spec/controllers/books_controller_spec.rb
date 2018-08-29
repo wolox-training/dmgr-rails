210ed75d95a7c7440579ca3d404d7729e21a409f
Droche <diego.rochebouet@gmail.com>
2018-08-29 16:37:03 -0300

    Adds rent factory and book-controller testing
    -Adds a factory for rents
    -Adds multiple tests for the book controller index and show routes

--- spec/controllers/books_controller_spec.rb
+++ spec/controllers/books_controller_spec.rb
@@ -1,42 +1,111 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 require 'controllers/authenticated_user'
 
 describe Api::V1::BooksController, type: :controller do
   include_context 'Authenticated User'
 
-  # describe 'GET #index' do
-  #   context 'When fetching all the book' do
-  #     let!(:index) { create_list(:rent, 3, user: user) }
-  #
-  #     it 'responses with the users rents json' do
-  #       expected = ActiveModel::Serializer::CollectionSerializer.new(
-  #         rents, each_serializer: RentSerializer
-  #       ).to_json
-  #       expect(response_body.to_json) =~ JSON.parse(expected)
-  #     end
-  #
-  #     it 'responds with 200 status' do
-  #       expect(response).to have_http_status(:ok)
-  #     end
-  #   end
-  # end
+  describe 'GET #index' do
+    context 'When fetching all the book' do
+      let(:books) { create_list(:book, 3) }
+
+      before do
+        get :index
+      end
+
+      it 'responses with the books jsons' do
+        expected = ActiveModel::Serializer::CollectionSerializer.new(
+          books, each_serializer: BookSerializer
+        ).to_json
+        expect(response.body.to_json) =~ JSON.parse(expected)
+      end
+
+      it 'responds with 200 status' do
+        expect(response).to have_http_status(:ok)
+      end
+    end
+  end
 
   describe 'GET #show' do
-    context 'When fetching a book' do
-      let!(:book) { create(:book) }
+    context 'When fetching a book that exist without rents' do
+      let(:book) { create(:book) }
 
       before do
         get :show, params: { id: book.id }
       end
 
-      it 'responses with the user rent json' do
-        expect(response_body.to_json).to eq BookSerializer.new(
+      it 'responses with the book json' do
+        expect(response.body).to eq BookShowSerializer.new(
           book, root: false
         ).to_json
       end
 
+      it 'responses with a book without rents' do
+        expect(ActiveSupport::JSON.decode(response.body)['actual_rent']).to be_nil
+      end
+
       it 'responds with 200 status' do
         expect(response).to have_http_status(:ok)
+      end
+    end
+
+    context 'When fetching a book that exist with a rent that is not current' do
+      let(:book) do
+        create(:book)
+      end
+      let(:rent) do
+        create(:rent, book: book, user: user)
+      end
+
+      before do
+        get :show, params: { id: book.id }
+      end
+
+      it 'responses with a book without rents' do
+        expect(ActiveSupport::JSON.decode(response.body)['actual_rent']).to be_nil
+      end
+
+      it 'responds with 200 status' do
+        expect(response).to have_http_status(:ok)
+      end
+    end
+
+    context 'When fetching a book that exist with a rent that is current' do
+      let(:book) do
+        create(:book)
+      end
+      let!(:rent) do
+        create(:rent, book: book, user: user, start_date: Time.zone.now - 10.months \
+          , end_date: Time.zone.now + 10.months)
+      end
+
+      before do
+        get :show, params: { id: book.id }
+      end
+
+      it 'responses with the book json' do
+        expect(response.body).to eq BookShowSerializer.new(
+          book, root: false
+        ).to_json
+      end
+
+      it 'responses with a book with rents' do
+        expect(ActiveSupport::JSON.decode(response.body)['actual_rent']).not_to be_nil
+      end
+
+      it 'responds with 200 status' do
+        expect(response).to have_http_status(:ok)
+      end
+    end
+
+    context 'When fetching a book that does not exist' do
+      before do
+        get :show, params: { id: 1 }
+      end
+
+      it 'responds with 400 status' do
+        expect(response).to have_http_status(:bad_request)
       end
     end
   end
--- spec/factories.rb
+++ spec/factories.rb
@@ -4,7 +4,7 @@ FactoryBot.define do
   factory :book do
     genre { Faker::HitchhikersGuideToTheGalaxy.location }
     author { Faker::Name.name }
-    image { Faker::String.random }
+    image { Faker::Internet.url }
     title { Faker::LordOfTheRings.location }
     publisher { Faker::Name.name }
     year { Faker::Number.number(4) }
@@ -13,8 +13,15 @@ FactoryBot.define do
   factory :user do
     first_name { Faker::Name.first_name }
     last_name { Faker::Name.last_name }
-    password { 123123 }
-    password_confirmation { 123123 }
-    email { Faker::Internet.email}
+    password { 123_123 }
+    password_confirmation { 123_123 }
+    email { Faker::Internet.email }
+  end
+
+  factory :rent do
+    user_id { Faker::Number.number(1000) }
+    book_id { Faker::Number.number(1000) }
+    start_date { Faker::Time.between(Time.now.utc - 1000.months, Time.now.utc + 1000.months) }
+    end_date { Faker::Time.between(Time.now.utc - 1000.months, Time.now.utc + 1000.months) }
   end
 end
