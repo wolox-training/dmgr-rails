fefb21fb96a41f11bc8ff70e461d37371745fcc5
Droche <diego.rochebouet@gmail.com>
2018-09-10 16:36:05 -0300

    Adds book sugestion model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180910125135) do
+ActiveRecord::Schema.define(version: 20180910185811) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,20 @@ ActiveRecord::Schema.define(version: 20180910125135) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_admin_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
+  end
+
+  create_table "book_sugestions", force: :cascade do |t|
+    t.string "editorial"
+    t.decimal "price"
+    t.string "author", null: false
+    t.string "title", null: false
+    t.string "link", null: false
+    t.string "publisher"
+    t.string "year"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_book_sugestions_on_user_id"
   end
 
   create_table "books", force: :cascade do |t|
@@ -84,6 +98,7 @@ ActiveRecord::Schema.define(version: 20180910125135) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "book_sugestions", "users"
   add_foreign_key "rents", "books"
   add_foreign_key "rents", "users"
 end
