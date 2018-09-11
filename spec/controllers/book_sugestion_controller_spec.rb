fd98150428e267ca1d2985997340e4c19c8232b5
Droche <diego.rochebouet@gmail.com>
2018-09-11 12:52:27 -0300

    Adds book sugestion controller

---
+++ spec/controllers/book_sugestion_controller_spec.rb
@@ -0,0 +1,45 @@
+require 'rails_helper'
+
+describe Api::V1::BookSugestionsController, type: :controller do
+  include_context 'Authenticated User'
+  describe 'POST #create' do
+    context 'When creating a valid book sugestion' do
+      let!(:new_book_sugestion_attributes) { title: FactoryBot::Name.name,
+                                             author: FactoryBot::Name.name,
+                                             link: FactoryBot::Internet.url}
+      it 'creates a new book sugestion' do
+        expect do
+          post :create, params: { book_sugestion: new_book_sugestion_attributes }
+        end.to change { BookSugestion.count }.by(1)
+      end
+
+      it 'responds with 201 status' do
+        post :create, params: { book_sugestion: new_book_sugestion_attributes }
+        expect(response).to have_http_status(:created)
+      end
+    end
+
+    context 'When creating an invalid user rent' do
+      let!(:new_book_sugestion_attributes) { title: FactoryBot::Name.name,
+                                             author: nil,
+                                             link: FactoryBot::Internet.url}
+      before do
+          post :create, params: { book_sugestion: new_book_sugestion_attributes }
+      end
+
+      it 'doesn\'t create a new rent' do
+        expect do
+          post :create, params: { book_sugestion: new_book_sugestion_attributes }
+        end.to change { BookSugestion.count }.by(0)
+      end
+
+      it 'returns error messages' do
+        expect(response_body['error']).to be_present
+      end
+
+      it 'responds with 422 status' do
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+    end
+  end
+end
---
+++ spec/factories/book_sugestion_factory.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+FactoryBot.define do
+  factory :book_sugestion do
+    editorial { Faker::HitchhikersGuideToTheGalaxy.location }
+    price { Faker::Number.decimal(2)}
+    author { Faker::Name.name }
+    title { Faker::LordOfTheRings.location }
+    link { Faker::Internet.url }
+    publisher { Faker::Name.name }
+    year { Faker::Number.number(4) }
+  end
+end
