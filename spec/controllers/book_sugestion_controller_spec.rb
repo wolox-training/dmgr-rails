9aab2bf23cac5aa44f1a5384b4ebcdeb7e56d808
Droche <diego.rochebouet@gmail.com>
2018-09-12 13:36:19 -0300

    Changes to testing book sugestion controller
    -Adds subject
    -Change to one liners
    -Changes let! to let

--- spec/controllers/book_sugestion_controller_spec.rb
+++ spec/controllers/book_sugestion_controller_spec.rb
@@ -4,35 +4,29 @@ require 'rails_helper'
 
 describe Api::V1::BookSugestionsController, type: :controller do
   describe 'POST #create' do
+    subject(:create_book_sugestion) { post :create, params: { book_sugestion: attributes } }
     context 'When creating a valid book sugestion' do
-      let!(:attributes) { FactoryBot.attributes_for(:book_sugestion) }
+      let(:attributes) { FactoryBot.attributes_for(:book_sugestion) }
 
       it 'creates a new book sugestion' do
-        expect do
-          post :create, params: { book_sugestion: attributes }
-        end.to change { BookSugestion.count }.by(1)
+        expect { create_book_sugestion }.to change { BookSugestion.count }.by(1)
       end
 
       it 'responds with 302 status' do
-        post :create, params: { book_sugestion: attributes }
+        create_book_sugestion
         expect(response).to have_http_status(:found)
       end
     end
 
     context 'When creating an invalid user rent' do
-      let!(:attributes) { FactoryBot.attributes_for(:book_sugestion, author: nil) }
-
-      before do
-        post :create, params: { book_sugestion: attributes }
-      end
+      let(:attributes) { FactoryBot.attributes_for(:book_sugestion, author: nil) }
 
       it 'doesn\'t create a new rent' do
-        expect do
-          post :create, params: { book_sugestion: attributes }
-        end.to change { BookSugestion.count }.by(0)
+        expect { create_book_sugestion }.to change { BookSugestion.count }.by(0)
       end
 
       it 'responds with 400 status' do
+        create_book_sugestion
         expect(response).to have_http_status(:bad_request)
       end
     end
